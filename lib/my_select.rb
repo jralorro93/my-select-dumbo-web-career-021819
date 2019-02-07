87b9cb944ca0e33f8d49811655a2917672e1ca89
Juan Alorro <jralorro93@gmail.com>
2019-02-07 16:08:54 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,3 @@
 def my_select(collection)
- if block_given?
-   i = 0 
-   while i < collection.length 
-   yield(collection)
-   i = i + 1 
- end 
- collection
- else 
-   puts "No block was given" 
- end 
-end
+
+end 
